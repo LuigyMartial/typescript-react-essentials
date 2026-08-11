2bed62328dd5799dcc31ba199d097810a02fd374
LuigyMartial <martial.luigy@gmail.com>
2026-08-11 15:32:01 +0200

    events and challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Component from './starter/04-events';
+import Component from './starter/06-context';
 
 function App() {
   return (
     <main>
+      <h2>React & Typescript</h2>
       <Component />
     </main>
   );
 }
+
 
 export default App;
