f01ad889274115bf5d93d38fb8328725b32d1caa
LuigyMartial <martial.luigy@gmail.com>
2026-08-12 14:45:16 +0200

    context

--- src/starter/06-context/index.tsx
+++ src/starter/06-context/index.tsx
@@ -1,9 +1,33 @@
+import { useTheme, ThemeProvider } from "./context";
+
 function Component() {
+  const context = useTheme();
+  console.log(context);
+
   return (
     <div>
       <h2>React & Typescript</h2>
-      <h2>Context API</h2>
+      <button onClick={() => {
+        if(context.theme === 'dark'){
+          context.setTheme('system');
+          return;
+        }
+        context.setTheme('dark');
+      }} 
+        className="btn btn-center"
+      >
+        toggle theme
+      </button>
     </div>
   );
 }
-export default Component;
+
+function ParentComponent(){
+  return (
+    <ThemeProvider>
+      <Component />
+    </ThemeProvider>
+  );
+}
+
+export default ParentComponent;
