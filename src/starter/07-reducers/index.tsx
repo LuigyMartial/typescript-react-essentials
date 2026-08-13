4cfee5c29ea1a25a2ab8352d4afad1954db21c1f
LuigyMartial <martial.luigy@gmail.com>
2026-08-13 18:56:52 +0200

    reducers

--- src/starter/07-reducers/index.tsx
+++ src/starter/07-reducers/index.tsx
@@ -1,8 +1,27 @@
+import { useReducer } from 'react';
+import { initialState, counterReducer } from './reducer';
+
 function Component() {
+  const [state, dispatch]= useReducer(counterReducer, initialState);
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>useReducer Hook</h2>
+      <h2>Count: {state.count}</h2>
+      <h2>Status: {state.status}</h2>
+
+      <div className="btn-container">
+        <button  className="btn"onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+        <button className="btn" onClick={() => dispatch({ type: 'decrement'})}>Decrement</button>
+        <button className="btn" onClick={() => dispatch({ type: 'reset'})}>Reset</button>
+      </div>
+      <div className="btn-container">
+        <button className="btn" onClick={() => dispatch({ type: 'setStatus', payload: 'active'})}>
+          Set status to Active
+        </button>
+        <button className="btn" onClick={() => dispatch({ type: 'setStatus', payload: 'inactive'})}>
+          Set status to Inactive
+        </button>
+      </div>
     </div>
   );
 }
