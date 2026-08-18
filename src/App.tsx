78b3ed3764b4a7cf74204fbaa98ba515e3612b1c
LuigyMartial <martial.luigy@gmail.com>
2026-08-18 16:14:47 +0200

    reduce tool kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import Component from './starter/07-reducers';
+import Component from './starter/09-rtk';
 
 function App() {
   return (
     <main>
-      <h2>React & Typescript</h2>
       <Component />
     </main>
   );
 }
-
 
 export default App;
